a74571e3ecd07c3ccc4b09f4760b9e3a9de2776a
joe9 <joe9mail@gmail.com>
2017-01-11 21:42:56 -0700

    compiling version using the existing C codegen code

--- src/backend/src/Main.hs
+++ src/backend/src/Main.hs
@@ -1,47 +1,51 @@
 module Main where
 
-import Idris.Core.TT
 import Idris.AbsSyntax
+import Idris.Core.TT
 import Idris.ElabDecls
-import Idris.REPL
 import Idris.Main
-import Idris.ModeCommon
-
-import IRTS.Compiler
 import IRTS.Codegen9FrontC
+import IRTS.Compiler
 
-import System.Environment
-import System.Exit
+import Util.System
 
 import Paths_idris_9frontc
 
+import Control.Monad
+import System.Environment
+import System.Exit
+
+
+
 data Opts = Opts { inputs :: [FilePath],
+                   interface :: Bool,
                    output :: FilePath }
 
-showUsage = do putStrLn "Usage: idris-9frontc <ibc-files> [-o <output-file>]"
+showUsage = do putStrLn "A code generator which is intended to be called by the compiler, not by a user."
+               putStrLn "Usage: idris-codegen-9frontc <ibc-files> [-o <output-file>]"
                exitWith ExitSuccess
 
 getOpts :: IO Opts
 getOpts = do xs <- getArgs
-             return $ process (Opts [] "a.out") xs
+             return $ process (Opts [] False "a.out") xs
   where
     process opts ("-o":o:xs) = process (opts { output = o }) xs
-    -- TODO Is this line needed?
-    process opts ("--yes-really":xs) = process opts xs -- GRRR
+    process opts ("--interface":xs) = process (opts { interface = True }) xs
     process opts (x:xs) = process (opts { inputs = x:inputs opts }) xs
     process opts [] = opts
 
 c_main :: Opts -> Idris ()
-c_main opts = do elabPrims
+c_main opts = do runIO setupBundledCC
+                 elabPrims
                  loadInputs (inputs opts) Nothing
-                 mainProg <- elabMain
-                 ir <- compile (Via IBCFormat "9frontc") (output opts) (Just mainProg)
+                 mainProg <- if interface opts
+                                then liftM Just elabMain
+                                else return Nothing
+                 ir <- compile (Via IBCFormat "9frontc") (output opts) mainProg
                  runIO $ codegen9FrontC ir
 
 main :: IO ()
 main = do opts <- getOpts
           if (null (inputs opts))
              then showUsage
-             else runMain (c_main opts)
-
-
+             else  runMain (c_main opts)

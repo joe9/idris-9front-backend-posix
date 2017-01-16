280050660f5ebbd1b3c9b7a11c1c1fe45d00d08a
joe9 <joe9mail@gmail.com>
2017-01-16 10:19:59 -0700

    updated test code

--- src/backend/test/hello.idr
+++ src/backend/test/hello.idr
@@ -1,7 +1,34 @@
 module Main
 
+data MyList : (elem : Type) -> Type where
+  MyNil : MyList elem
+  MyCons : elem -> MyList elem -> MyList elem
+
+MyList1 : Type
+MyList1 = MyList Integer
+
+append : {a : Type} -> MyList a -> MyList a -> MyList a
+append MyNil ys = ys
+append (MyCons x xs) ys = MyCons x (append xs ys)
+
+append1 : MyList1 -> MyList1 -> MyList1
+append1 MyNil ys = ys
+append1 (MyCons x xs) ys = MyCons x (append xs ys)
+
+showMyList : MyList Integer -> String
+showMyList MyNil = "Nil"
+showMyList (MyCons x xs) = "MyCons " ++ show x  ++ " " ++ showMyList xs
+
+myFun : MyList Integer -> Integer -> Integer
+myFun MyNil j = j
+myFun (MyCons x xs) j = x + myFun xs j
+
 main : IO ()
 main = do
   putStrLn "Hello world"
   putStrLn (show (1000 + 2000))
   putStrLn (show (100012323222232323223232223223232 + 2 + 3))
+  putStrLn (showMyList (append (MyCons 10 MyNil) (MyCons 20 (MyCons 30 MyNil))))
+  putStrLn (show (myFun (MyCons 10 MyNil) 20))
+
+
--- src/rts/testmp.c
+++ src/rts/testmp.c
@@ -3,11 +3,74 @@
 #include <libc.h>
 #include <mp.h>
 
+struct Constructor {
+    int ctype;
+    void *arguments; /* pointer to an array of arguments */
+};
+
+typedef struct Constructor Constructor;
+
+Constructor *
+append (int i, int j, Constructor *returnValue) {
+    int is = {i,j};
+    is = ;
+    returnValue->arguments[0] = i;
+    returnValue->arguments[1] = j;
+    return returnValue;
+}
+
+void
+testAppend (void) {
+    int *arguments;
+    Constructor returnValue;
+    int i = 10;
+
+    print("arguments pointer : %p\n",arguments);
+    if (nil == (arguments = calloc (2,sizeof(int)))) {
+	sysfatal("no memory");
+    }
+    returnValue.ctype = 1;
+    returnValue.arguments = (void*)arguments;
+
+    returnValue = *append((int)10,(int)20,&returnValue);
+    print("return value : %d\n",returnValue.ctype);
+    print("return value : %d\n",i*-1);
+
+}
+
 void
 main (int, char* [])
 {
+    {
+	int x = 10, y  = 20;
+	{
+	    // The outer block contains declaration of x and y, so
+	    // following statement is valid and prints 10 and 20
+	    print("x = %d, y = %d\n", x, y);
+	    {
+		// y is declared again, so outer block y is not accessible
+		// in this block
+		int y = 40;
+
+		x++;  // Changes the outer block variable x to 11
+		y++;  // Changes this block's variable y to 41
+
+		print("x = %d, y = %d\n", x, y);
+	    }
+
+	    // This statement accesses only outer block's variables
+	    print("x = %d, y = %d\n", x, y);
+	}
+    }
+    testAppend();
+    exits(nil);
+}
+
+void
+main1 (int, char* [])
+{
     mpint i;
-    mpint *m = nil;
+    mpint *m;
     char *s = "12\0";
     char *r = nil;
 
@@ -28,3 +91,8 @@ main (int, char* [])
     print("testmp exiting\n");
     exits(nil);
 }
+
+/* linked list implementation */
+/* struct Myshit { int foo; int bar; .... Myshit *next; }. */
+/* Myshit *p; .... for(p = myshitlist; p != nil; p = p->next) { ... } */
+/* adding shit to a linked list: newshit->next = myshitlist; myshitlist = newshit */
